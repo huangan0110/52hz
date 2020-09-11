78e207ef7d2fd72188046f7a71e13d3e3323b6bc
huangan0110 <877347136@qq.com>
2020-09-11 21:50:14 +0800

    create project

---
+++ src/view/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="Index">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+    components: {
+    }
+}
+</script>
+
+<style lang="scss" socped>
+</style>
